6d0242039687806826777459595189d6aed1ae91
juliangamble <juliangamble@192.168.0.12>
2014-02-03 01:07:53 -0800

    Added in afterburner tier assets & functionality.

--- src/GraphicsMarshal.java
+++ src/GraphicsMarshal.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+import playerShip.AfterburnerType;
 import playerShip.ShipVector;
 
 
@@ -15,12 +16,16 @@ public class GraphicsMarshal {
 	public Image trackingWall;
 	public Image playerBasicProjectile;
 	
-	public Animation jetAfterburner;
+	public Animation jetAfterburnerTier1;
+	public Animation jetAfterburnerTier2;
+	public Animation jetAfterburnerTier3;
 	
 	private Hashtable<ShipVector, Image> playerShip;
+	private Hashtable<AfterburnerType, Animation> afterburner;
 	
 	public GraphicsMarshal () {
 		playerShip = new  Hashtable<ShipVector, Image>();
+		afterburner = new Hashtable<AfterburnerType, Animation>();
 		
 		try {
 			playerShipIdle = new Image("resources/Jet_A.png");
@@ -30,12 +35,20 @@ public class GraphicsMarshal {
 			trackingWall = new Image("resources/VerticalLine.png");
 			playerBasicProjectile = new Image("resources/Projectile_Small.png");
 
-	    	Image[] aferburnerAnim = {new Image("resources/Afterburner_1.png"), new Image("resources/Afterburner_2.png")};
-	    	jetAfterburner = new Animation(aferburnerAnim, 200);
+	    	Image[] aferburnerAnimT1 = {new Image("resources/Afterburner_1.png"), new Image("resources/Afterburner_2.png")};
+	    	jetAfterburnerTier1 = new Animation(aferburnerAnimT1, 200);
+	    	Image[] aferburnerAnimT2 = {new Image("resources/Afterburner_1_T2.png"), new Image("resources/Afterburner_2_T2.png")};
+	    	jetAfterburnerTier2 = new Animation(aferburnerAnimT2, 200);
+	    	Image[] aferburnerAnimT3 = {new Image("resources/Afterburner_1_T3.png"), new Image("resources/Afterburner_2_T3.png")};
+	    	jetAfterburnerTier3 = new Animation(aferburnerAnimT3, 200);
 			
 			playerShip.put(ShipVector.LEFT, playerShipLeanLeft);
 			playerShip.put(ShipVector.RIGHT, playerShipLeanRight);
 			playerShip.put(ShipVector.CENTER, playerShipIdle);
+			
+			afterburner.put(AfterburnerType.TIER_1, jetAfterburnerTier1);
+			afterburner.put(AfterburnerType.TIER_2, jetAfterburnerTier2);
+			afterburner.put(AfterburnerType.TIER_3, jetAfterburnerTier3);
 			
 		} catch (SlickException e) {
 			// TODO Auto-generated catch block
@@ -59,7 +72,7 @@ public class GraphicsMarshal {
 		return playerBasicProjectile;
 	}
 	
-	public Animation getPlayerAfterburner() {
-		return jetAfterburner;
+	public Animation getPlayerAfterburner(int burnFactor) {
+		return afterburner.get(AfterburnerType.values()[burnFactor]);
 	}
 }
---
+++ src/playerShip/AfterburnerType.java
@@ -0,0 +1,6 @@
+package playerShip;
+
+public enum AfterburnerType {
+	
+	  TIER_0, TIER_1, TIER_2, TIER_3;
+}
