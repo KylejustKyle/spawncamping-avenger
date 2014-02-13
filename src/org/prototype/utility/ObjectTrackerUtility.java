9d17719b740cd00e1db4033e9881447184020c3c
juliangamble <juliangamble@192.168.0.12>
2014-02-12 23:04:34 -0800

    Wall stitching first pass.

--- src/org/prototype/utility/ObjectTrackerUtility.java
+++ src/org/prototype/utility/ObjectTrackerUtility.java
@@ -9,7 +9,7 @@ public class ObjectTrackerUtility {
 	
 	// @TODO a lot of class heirarchy can go here to help make this more autowired.
 	public static boolean isOutsideOfWindow(WorldObject object) {
-		return isOutsideOfWindow(object.x, object.y);
+		return isLeavingBottom(object.x, object.y);
 	}
 	
 	public static boolean isOutsideOfWindow(CollidableObject object) {
@@ -24,6 +24,14 @@ public class ObjectTrackerUtility {
 		if(x < 0 || x >WIDTH) {
 			return true;
 		} else if( y < 0 || y > HEIGHT) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private static boolean isLeavingBottom(float x, float y) {
+		if(y > HEIGHT) {
 			return true;
 		} else {
 			return false;
--- src/org/prototype/world/entities/WorldObject.java
+++ src/org/prototype/world/entities/WorldObject.java
@@ -3,9 +3,12 @@ package org.prototype.world.entities;
 public class WorldObject {
 	public float x;
 	public float y;
+	public boolean hasPassedOrigin;
+	public boolean hasFollowingWall = false;
 	
 	public WorldObject(float xPosition, float yPosition) {
 		x = xPosition;
 		y = yPosition;
+		hasPassedOrigin = y >=0 ? true : false;
 	}
 }
