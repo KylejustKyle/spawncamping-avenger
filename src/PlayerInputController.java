49e26fd1b43985a675146f7835ffdad22999d6b7
juliangamble <juliangamble@192.168.0.12>
2014-02-14 00:56:01 -0800

    Added stub background for prep on moving background. Added fix on bullet timer.

--- src/PlayerInputController.java
+++ src/PlayerInputController.java
@@ -3,6 +3,7 @@ import org.prototype.player.MockPlayer;
 import org.prototype.player.ShipVector;
 import org.prototype.projectiles.VerticalProjectile;
 import org.prototype.projectiles.WorldProjectiles;
+import org.prototype.utility.IntervalTimer;
 
 
 
@@ -10,13 +11,15 @@ public class PlayerInputController {
 	
 	public int screenWidth;
 	public int screenHeight;
+	private static IntervalTimer inputTimer;
 	
 	public PlayerInputController(int screenWidth, int screenHeight) {
 		this.screenHeight = screenHeight;
 		this.screenWidth = screenWidth;
+		inputTimer = new IntervalTimer(200);
 	}
 	
-	public GameState consumeInput(MockPlayer player, GameState currentState, int delta) {
+	public GameState consumeInput(MockPlayer player, GameState currentState, int delta, WorldProjectiles wProjectiles) {
 		// Check for general keystrokes first
 		if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 			currentState = GameState.END;
@@ -25,7 +28,7 @@ public class PlayerInputController {
 		
 		switch(currentState) {
 			case IN_FLIGHT: 
-				consumeInFlightInput(player, delta);
+				consumeInFlightInput(player, delta, wProjectiles);
 			case MENU:
 				consumeInMenuInput();
 		}
@@ -44,16 +47,24 @@ public class PlayerInputController {
 	}
 	
 	public void fireControls(MockPlayer player, WorldProjectiles wProjectiles) {
-		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-			//Projectile factory should be used here instead.
-			wProjectiles.wProjectiles.add(new VerticalProjectile(1, 1, player.x+player.middle, player.y, 10, 10));
-		}
+//		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+//			//Projectile factory should be used here instead.
+//			wProjectiles.wProjectiles.add(new VerticalProjectile(1, 1, player.x+player.middle, player.y, 10, 10));
+//		}
 	}
 	
-	private void consumeInFlightInput(MockPlayer player, int delta) {
+	private void consumeInFlightInput(MockPlayer player, int delta, WorldProjectiles wProjectiles) {
 		//We don't want to run this consuming flight control logic if the player is dead.
 		if(!player.isAlive) {
 			return;
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			//Projectile factory should be used here instead.
+			if(inputTimer.isInterval()) {
+				wProjectiles.wProjectiles.add(new VerticalProjectile(1, 1, player.x+player.middle, player.y, 10, 10));
+				inputTimer.rootTime = System.currentTimeMillis();
+			}
 		}
 		
         if(Keyboard.isKeyDown(Keyboard.KEY_S) && player.y+player.height < screenHeight) {
